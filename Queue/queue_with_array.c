2605455ca9346f2d20089be9d0075d1d78964154
Dursun <93613110+Dursunipek@users.noreply.github.com>
2022-07-26 18:46:37 +0300

    queue_with_array.c problem solved

--- Queue/queue_with_array.c
+++ Queue/queue_with_array.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 typedef struct q{
     int *array;
     int head;
@@ -7,82 +8,70 @@ typedef struct q{
     int size;
 }que;
 
-que* init(que *q){
+que * init(que *q){
+    q = (que*)malloc(sizeof(que));
     q -> array = NULL;
     q -> head = 0;
     q -> end = 0;
     q -> size = 2;
+    return q;
 }
 
 int deque(que *q){
     if(q -> head == q -> end){ // Sirada eleman yoksa bu islem uygulanir
-        printf("Queue is empty");
+        printf("Queue is empty!!\n");
         return -1;
     }
-    if((q -> end) - (q -> head) <= q -> size /4 ){
+    if(((q -> end) - (q -> head) + 1) <= q -> size /4 ){ // Eger dizi boyutu gereksiz derecede buyuk ise kucultme yapilir
         q -> size /= 2;
         int *array2 = (int*)malloc(sizeof(int)*q->size);
         for(int i=0;i < q->size;i++){
-            array2[i] = q -> array[i];
+            array2[i] = q -> array[i + q -> head];
         }
+        q -> end -= q -> head;
+        q -> head = 0;
         free(q -> array);
         q -> array = array2;
     }
     return q -> array[q -> head++]; // Eleman varsa cıkarilan elemanı geri dondur ver siranin basini 1 arttir
-}
-void clearQue(que *q){ // Siradaki boslukları temizleyen bir fonksiyon
-    if(q -> head == 0){
-        return ;
-    }
-    for(int i = 0; i < q -> size;i++){
-        q -> array[i] = q -> array[i + q -> head];
-    }
-    q -> end -= q -> head;
-    q -> head = 0;
 }
 
 void enque(int data, que *q){
     if(q -> array == NULL){ // Eger sira bos ise yeni bir alan ayir
         q -> array = (int*)malloc(sizeof(int)* q -> size);
     }
-    if(q -> end >= q -> size){ // Eger dizinin eleman sayisi dolmussa eleman sayisini 2 katina cikar 
+    if((q -> end + 1) >= q -> size){ // Eger dizinin eleman sayisi dolmussa eleman sayisini 2 katina cikar 
         q -> size *= 2;
         int *array2 = (int*)malloc(sizeof(int)* q-> size); // Yeni bir dizi olusturarak elemanları bu diziye aktarıyoruz
         for(int i=0;i < q -> size / 2; i++){
-            array2[i] = q -> array[i];
+            array2[i] = q -> array[i + q -> head];
         }
         free(q -> array); // Eski diziyi free yaptiktan sonra yeni dizimizi eski dizimize atiyoruz
         q -> array = array2;
     }
-    q -> array[q -> end++] = data; // Eger dizide bir sorun yoksa yeni gelen elemanı son indise ata ve dizinin  sonunu 1 arttir
+    q -> array[q -> end++] = data; // Eger dizide bir sorun yoksa yeni gelen elemanı son indise ekler
 }
 
 void printQue(que *q){
-    for(int i = 0; i < q -> end;i++){
+    for(int i = q -> head ; i < q -> end;i++){
         printf("%d. index -> %d\n",i,q -> array[i]);
     }
 }
 
 int main(){
     que *q1 = init(q1);
-    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end);
-    for(int i = 0; i < 20 ; i++)
+    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end); 
+    for(int i = 0; i < 10 ; i++){
         enque(i*10,q1);
-    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end);
+    }
     printQue(q1);
-    clearQue(q1);
-    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end);
+    for(int i = 0; i < 10 ; i++){
+        deque(q1);
+    }
+    deque(q1);
+    deque(q1);
+    deque(q1);
     printQue(q1);
-    for(int i = 0; i < 10 ; i++)
-        deque(q1);
-    clearQue(q1);
-    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end);
     printQue(q1);
-    for(int i = 0; i < 2 ; i++)
-        deque(q1);
-/*     for(int i = 0; i < 5 ; i++)
-        deque(q1);
-    clearQue(q1);
-    printf("Size -> %d Head -> %d End -> %d\n",q1 -> size, q1 -> head, q1 -> end);
-    printQue(q1); */
+
 }
